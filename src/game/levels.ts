cf8568ea0da96b43b58dd222e7410fba743d952f
marc-2019 <54982126+marc-2019@users.noreply.github.com>
2026-08-04 16:10:24 +1000

    Quota overrides + levels apply path (L1 12 sells through L18 140)

--- src/game/levels.ts
+++ src/game/levels.ts
@@ -1,5 +1,6 @@
 import type { LevelDef } from "./types";
 import { LEVELS_ACT12 } from "./levels_act12";
 import { LEVELS_ACT345 } from "./levels_act345";
+import { applyQuotaOverrides } from "./quotaOverrides";
 
-export const LEVELS: LevelDef[] = [...LEVELS_ACT12, ...LEVELS_ACT345];
+export const LEVELS: LevelDef[] = applyQuotaOverrides([...LEVELS_ACT12, ...LEVELS_ACT345]);
